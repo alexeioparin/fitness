caaab9bc35885d84d821d073d2eed1686e736f48
alexeioparin <alexeioparin@gmail.com>
2020-10-03 19:18:39 +0300

    Верстка сайта

--- gulpfile.js
+++ gulpfile.js
@@ -66,11 +66,11 @@ gulp.task("webp", function () {
     .pipe(gulp.dest("source/img"));
 });
 
-gulp.task("sprite", function () {
-  return gulp.src("source/img/{icon-*,htmlacademy*}.svg")
-    .pipe(svgstore({inlineSvg: true}))
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
+gulp.task("sprite", function() {
+  return gulp.src("source/img/icons/icon-*.svg")
+  .pipe(svgstore({inlineSvg: true}))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"))
 });
 
 gulp.task("html", function () {
