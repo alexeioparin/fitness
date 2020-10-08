a388e9eae6a7c32064bb57baba6178d9b035dfd8
alexeioparin <alexeioparin@gmail.com>
2020-10-08 12:55:05 +0300

    Внесение правок

--- build/js/script.js
+++ build/js/script.js
@@ -1,18 +1,18 @@
 'use strict';
 
-let menuButton = document.querySelector('.header__button');
+let onCkickShowMenu = document.querySelector('.header__button');
 let headerBlock = document.querySelector('.header');
 let mainBlock = document.querySelector('.main-content');
 let introBlock = document.querySelector('.intro');
 
 headerBlock.classList.remove('menu-opened');
-menuButton.classList.remove('header__button--nojs');
+onCkickShowMenu.classList.remove('header__button--nojs');
 mainBlock.classList.remove('menu-opened');
 introBlock.classList.remove('menu-opened');
 
-menuButton.addEventListener('click', function() {
+onCkickShowMenu.addEventListener('click', function() {
   headerBlock.classList.toggle('menu-opened');
-  menuButton.classList.toggle('button-active');
+  onCkickShowMenu.classList.toggle('button-active');
   mainBlock.classList.toggle('menu-opened');
   introBlock.classList.toggle('menu-opened');
 })
