3935d44d28a24a603b411a51bb4f5fed128b4d8a
Spin <alexeioparin@gmail.com>
2020-11-01 00:11:01 +0300

    pp

--- source/js/script.js
+++ source/js/script.js
@@ -2,26 +2,51 @@
 
 let abonMenu = document.querySelector('.abonements__menu');
 let abonMenuItems = abonMenu.querySelectorAll('li button');
-let slider = document.querySelector('.trainers__slider');
-let prevButton = document.querySelector('.trainers__arrow-prev');
-let nextButton = document.querySelector('.trainers__arrow-next');
-let currentShift = 0;
+let trainersSlider = document.querySelector('.trainers__slider');
+let trainerPrevButton = document.querySelector('.trainers__arrow-prev');
+let trainerNextButton = document.querySelector('.trainers__arrow-next');
+let trainersCurrentShift = 0;
+let feedbackSlider = document.querySelector('.feedback__slider');
+let feedbackPrevButton = document.querySelector('.feedback__arrow-prev');
+let feedbackNextButton = document.querySelector('.feedback__arrow-next');
+let feedbackCurrentShift = 0;
+
+let shiftOnPrevButton = function (shiftVar, elem) {
+  if (shiftVar < 0) {
+    shiftVar += 50;
+    elem.style.transform = 'translate(' + shiftVar + '%)';
+  }
+}
 
 abonMenu.addEventListener('click', function(evt) {
   abonMenuItems.forEach(element => element.classList.remove('abonements__active-item'));
   evt.target.classList.add('abonements__active-item');
 });
 
-prevButton.addEventListener('click', function() {
-  if (currentShift < 0) {
-    currentShift += 12.5;
-    slider.style.transform = 'translate(' + currentShift + '%)';
+trainerPrevButton.addEventListener('click', function() {
+  if (trainersCurrentShift < 0) {
+    trainersCurrentShift += 50;
+    trainersSlider.style.transform = 'translate(' + trainersCurrentShift + '%)';
   }
 })
 
-nextButton.addEventListener('click', function() {
-  if (currentShift > -50) {
-    currentShift -= 12.5;
-    slider.style.transform = 'translate(' + currentShift + '%)';
+trainerNextButton.addEventListener('click', function() {
+  if (trainersCurrentShift > -50) {
+    trainersCurrentShift -= 50;
+    trainersSlider.style.transform = 'translate(' + trainersCurrentShift + '%)';
+  }
+})
+
+feedbackPrevButton.addEventListener('click', function() {
+  if (feedbackCurrentShift < 0) {
+    feedbackCurrentShift += 50;
+    feedbackSlider.style.transform = 'translate(' + feedbackCurrentShift + '%)';
+  }
+})
+
+feedbackNextButton.addEventListener('click', function() {
+  if (feedbackCurrentShift > -50) {
+    feedbackCurrentShift -= 50;
+    feedbackSlider.style.transform = 'translate(' + feedbackCurrentShift + '%)';
   }
 })
