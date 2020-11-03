282dbd75f62408960e62e6b413707c45847dd0df
Spin <alexeioparin@gmail.com>
2020-11-03 16:46:13 +0300

    Завершение проекта

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,7 @@
 let abonMenu = document.querySelector('.abonements__menu');
 let abonMenuItems = abonMenu.querySelectorAll('li button');
 let trainersSlider = document.querySelector('.trainers__slider');
+let trainersSliderMask = document.querySelector('.trainers__mask');
 let trainerPrevButton = document.querySelector('.trainers__arrow-prev');
 let trainerNextButton = document.querySelector('.trainers__arrow-next');
 let trainersCurrentShift = 0;
@@ -11,30 +12,47 @@ let feedbackPrevButton = document.querySelector('.feedback__arrow-prev');
 let feedbackNextButton = document.querySelector('.feedback__arrow-next');
 let feedbackCurrentShift = 0;
 
-let shiftOnPrevButton = function (shiftVar, elem) {
-  if (shiftVar < 0) {
-    shiftVar += 50;
-    elem.style.transform = 'translate(' + shiftVar + '%)';
+let shiftLeft = function() {
+  if (trainersCurrentShift < -50) {
+    let shift = getComputedStyle(trainersSliderMask).width;
+    trainersCurrentShift += Number(shift.slice(0, -2));
+    trainersSlider.style.transform = 'translate(' + trainersCurrentShift + 'px)';
   }
 }
 
+let shiftRight = function() {
+  let shift = getComputedStyle(trainersSliderMask).width;
+  if (trainersCurrentShift > -Number(getComputedStyle(trainersSlider).width.slice(0, -2)) + Number(shift.slice(0, -2)) + 100) {
+    trainersCurrentShift -= Number(shift.slice(0, -2));
+    trainersSlider.style.transform = 'translate(' + trainersCurrentShift + 'px)';
+  }
+}
+
+
 abonMenu.addEventListener('click', function(evt) {
-  abonMenuItems.forEach(element => element.classList.remove('abonements__active-item'));
+  for (let i = 0; i < abonMenuItems.length; i++) {
+    abonMenuItems[i].classList.remove('abonements__active-item');
+  }
   evt.target.classList.add('abonements__active-item');
 });
 
-trainerPrevButton.addEventListener('click', function() {
-  if (trainersCurrentShift < 0) {
-    trainersCurrentShift += 50;
-    trainersSlider.style.transform = 'translate(' + trainersCurrentShift + '%)';
-  }
-})
+trainerPrevButton.addEventListener('click', shiftLeft);
 
-trainerNextButton.addEventListener('click', function() {
-  if (trainersCurrentShift > -50) {
-    trainersCurrentShift -= 50;
-    trainersSlider.style.transform = 'translate(' + trainersCurrentShift + '%)';
+trainerNextButton.addEventListener('click', shiftRight);
+
+trainersSlider.addEventListener('mousedown', function(evtStart) {
+  evtStart.preventDefault();
+  let startCoords = evtStart.clientX;
+  let onMouseUp = function(evtEnd) {
+    let endCoords = evtEnd.clientX;
+    if (startCoords < endCoords + 50) {
+      shiftLeft();
+    } else if (startCoords > endCoords - 50) {
+      shiftRight();
+    }
+    trainersSlider.removeEventListener('mouseup', onMouseUp);
   }
+  trainersSlider.addEventListener('mouseup', onMouseUp);
 })
 
 feedbackPrevButton.addEventListener('click', function() {
